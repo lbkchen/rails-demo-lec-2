48b2f0189c9c6622d91fa764af673651a4ad2e43
Ken Chen <lbkchen@gmail.com>
2017-09-20 15:40:36 -0700

    Start fresh for demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  root 'countries#index'
-  resources :countries, only: [:index, :new, :create] do
-    resources :cheeses, only: [:index, :new, :create]
-  end
+  
 end
